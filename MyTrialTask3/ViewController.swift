233bf57a408b3f526f2ee7e5316af5746fdff043
Sergey Sklyarov <sklyarov.it.020@gmail.com>
2021-07-05 17:06:01 +0200

    Initial Commit

---
+++ MyTrialTask3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTrialTask3
+//
+//  Created by Wizzard Sklyarov on 2021. 07. 05..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
